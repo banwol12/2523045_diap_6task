22297659c9560049c98dcac0f46c3246ee8b3715
banwol12 <gkxkddl@gmail.com>
2025-11-16 23:15:21 +0900

    turb 값 조절

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
   background(51);
 
-  let turbulence = createVector(random(-0.3, 0.3), random(-0.3, 0.3));
+  let turbulence = createVector(random(-0.1, 0.1), random(-0.1, 0.1));
   
   system.applyForce(turbulence);
   system.addParticle();
